cc0e1a3cb0384806911a3fa5c168a12e4f24e569
Hawraa <hawra8516@gmail.com>
2025-09-21 15:19:19 +0400

    Set Questions: Question 1 added

---
+++ EventSecurity.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Arrays;
+
+class EventSecurity {
+    private HashSet<String> vipList = new HashSet<>(Arrays.asList("Alice", "Bob", "Charlie"));
+    /**
+     * TO-DO: Check if the attendee list contains anyone NOT in the vipList.
+     * \*
+     *
+     * @param attendees A list of names to check.
+     * @return true if a non-VIP is found, false if all attendees are VIPs.
+     */
+    public boolean containsNonVip(List<String> attendees) {
+        for (String attendee : attendees) {
+            if (!vipList.contains(attendee)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
